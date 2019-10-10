433022dd73fc65c88eb227f81465d6fc18872991
hgeng21 <45079537+hgeng21@users.noreply.github.com>
2019-10-10 17:01:26 -0600

    done
    Find name and phone# of persons who have had a 1969 Chevrolet Camaro registered under their names at some point.

--- Assignment2/q1.sql
+++ Assignment2/q1.sql
@@ -1,3 +1,6 @@
-SELECT fname,lname
-FROM registrations r,vehicles v
-WHERE r.vin=v.vin and v.make="Chevrolet" and v.model="Camaro" and v.year=1969;
+.headers ON
+.mode column
+.print Query1: Find <name> and <phone#> of persons who have had a 1969 Chevrolet Camaro registered under their names at some point.
+SELECT p.fname,p.lname,p.phone
+FROM persons p,registrations r,vehicles v
+WHERE r.vin=v.vin and v.make="Chevrolet" and v.model="Camaro" and v.year=1969 and p.fname=r.fname and p.lname=r.lname;
