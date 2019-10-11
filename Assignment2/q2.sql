9dede7d84ce44554dae5d9fadaa46771d93e170d
hgeng21 <45079537+hgeng21@users.noreply.github.com>
2019-10-10 18:52:37 -0600

    done
    Find the names of people other than Michael Fox who are born to the same parent as Michael Fox.
    (either same mother or same father)

--- Assignment2/q2.sql
+++ Assignment2/q2.sql
@@ -1,6 +1,17 @@
-.headers ON
-.mode column
 .print Query2: Find the <names> of people other than <Michael Fox> who are born to the <same parent> as Michael Fox.
 SELECT b2.fname,b2.lname
 FROM births b1, births b2
-WHERE b1.fname="Mickey" and b1.lname="Mouse" and b2.f_fname=b1.f_fname and b2.f_lname=b1.f_lname;
+WHERE b1.fname="Michael" and b1.lname="Fox" and b2.f_fname=b1.f_fname and b2.f_lname=b1.f_lname
+
+INTERSECT
+
+SELECT b2.fname,b2.lname
+FROM births b1, births b2
+WHERE b1.fname="Michael" and b1.lname="Fox" and b2.m_fname=b1.m_fname and b2.m_lname=b1.m_lname
+
+Except
+
+SELECT b2.fname,b2.lname
+FROM births b1, births b2
+WHERE (b2.fname||""||b2.lname)="MichaelFox"
+;
